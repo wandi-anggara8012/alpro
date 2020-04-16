dccb96305d6a54750e5d43e41bd026db7f20d400
wandi-anggara8012 <63702161+wandi-anggara8012@users.noreply.github.com>
2020-04-16 18:48:57 +0800

    Create tuggas 5 taypedefprogram.cpp

---
+++ tuggas 5 taypedefprogram.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+
+	typedef int integer;
+	integer num1, num2, sum;
+	cout<<"Masukkan Dua Angka : ";
+	cin>>num1>>num2;
+	sum=num1+num2;
+	cout<<"Total = "<<sum;
+
+}
